301de4150cc05241897247361ac6701618e66309
Nikolina <71445284+nikolina2k@users.noreply.github.com>
2023-04-08 12:12:04 +0300

    initialize project

---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './app';
+
+export default () => <App/>;
+
+export const mount = (Сomponent, element = document.getElementById('app')) => {
+  const root = ReactDOM.createRoot(element);
+  root.render(<Сomponent/>);
+
+  if(module.hot) {
+    module.hot.accept('./app', ()=> {
+      root.render(<Сomponent/>);
+    })
+  }
+};
+
+export const unmount = () => {
+  ReactDOM.unmountComponentAtNode(document.getElementById('app'));
+};
+
