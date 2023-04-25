552cfbf4a9990235128dea0614a5c20992926d57
Nikolina <71445284+nikolina2k@users.noreply.github.com>
2023-04-25 21:45:52 +0300

    turn over new leaf

--- src/pages/Account/index.tsx
+++ src/pages/Account/index.tsx
@@ -1,17 +1,15 @@
-import React from 'react'
+import React from 'react';
 import { useParams } from 'react-router-dom';
+import Accounts from '../../components/Accounts';
+import './Account.css';
 
-export default function Account () {
-    const {address} = useParams()
-    console.log({address})
-    return (
-        <>
-        <h1>Account</h1>
-        <p>TODO: have an input form where we can enter the address and filter transactions, show positions</p>
-        TODO: if account is null, just show a table of top liquidity positions with columns (account - pair - value)
-        TODO: else show button that takes you to explorer, positions table for the account, and then (maybe) transactions table
-        <p>Test: {address}</p>
+export default function Account() {
+  const { address } = useParams();
+  console.log({ address });
 
-        </>
-    );
+  return (
+    <div className="account-container">
+      <Accounts account={'some string'} />
+    </div>
+  );
 }
--- src/pages/Tokens/index.tsx
+++ src/pages/Tokens/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import PairsTable, { Pair } from '../../components/PairsTabe'
+import './TokenList.css';
 
 export default function TokenList() {
-    return (
-        <>
-            <h1>Tokens</h1>
-            <p>TODO: copy the same table from the home page, or instead of copying, create a Pairs Table component that we can use both at Home and in here</p> 
-        </>
-    )
+  const pairs: Pair[] = [
+    { name: 'ETH/BTC', symbol: 'ETHBTC', volume: 123456.789, liquidity: 987654.321 },
+    { name: 'BTC/USDT', symbol: 'BTCUSDT', volume: 987654.321, liquidity: 123456.789 },
+    // ...
+  ];
+
+  return (
+    <div className="token-list-container">
+      <PairsTable pairs={pairs} />
+    </div>
+  );
 }
