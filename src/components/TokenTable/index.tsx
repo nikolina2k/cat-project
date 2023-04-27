467961e4216d98427b68fd4c5b7fe8de6d1df922
Nikolina <71445284+nikolina2k@users.noreply.github.com>
2023-04-27 23:48:02 +0300

    remove style

--- src/components/TokenTable/index.tsx
+++ src/components/TokenTable/index.tsx
@@ -1,38 +1,43 @@
-import React from 'react';
-import { Space, Table, Tag } from 'antd';
-import { Image } from 'antd';
+import React from "react";
+import { Space, Table, Tag } from "antd";
+import { Image } from "antd";
 
 const { Column } = Table;
 
 interface TokenInfo {
-  name: string,
-  icon_url: string
+  name: string;
+  icon_url: string;
 }
 
 interface TokenTableEntry {
   key: React.Key;
-  tokenInfo: TokenInfo,
-  volume24: number,
-  symbol: string,
-  liquidity: number,
-  totalSupply: number
+  tokenInfo: TokenInfo;
+  volume24: number;
+  symbol: string;
+  liquidity: number;
+  totalSupply: number;
 }
 
-function TokenTable({data}) {
-    return (
-        <Table dataSource={data} style={{width: "100%", margin: 25}}>
-          <Column title="Name" dataIndex="tokenInfo" key="tokenInfo" render={(tokenInfo: TokenInfo) => (
-              <>
-              <Image height={32} src={tokenInfo.icon_url} /> {tokenInfo.name}
-              </>
-            )}/>
-          <Column title="Volume (24 h)" dataIndex="volume24" key="volume24" />
-          <Column title="Symbol" dataIndex="symbol" key="symbol" />
-          <Column title="Liquidity" dataIndex="liquidity" key="liquidity" />
-          <Column title="Total supply" dataIndex="totalSupply" key="totalSupply" />
-        </Table>
-    );
- }
+function TokenTable({ data }) {
+  return (
+    <Table dataSource={data}>
+      <Column
+        title="Name"
+        dataIndex="tokenInfo"
+        key="tokenInfo"
+        render={(tokenInfo: TokenInfo) => (
+          <>
+            <Image height={32} src={tokenInfo.icon_url} /> {tokenInfo.name}
+          </>
+        )}
+      />
+      <Column title="Volume (24 h)" dataIndex="volume24" key="volume24" />
+      <Column title="Symbol" dataIndex="symbol" key="symbol" />
+      <Column title="Liquidity" dataIndex="liquidity" key="liquidity" />
+      <Column title="Total supply" dataIndex="totalSupply" key="totalSupply" />
+    </Table>
+  );
+}
 
 export default TokenTable;
-export type { TokenTableEntry }
+export type { TokenTableEntry };
