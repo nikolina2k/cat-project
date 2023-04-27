ebaebfa7e05d372d12b8afaa4f81b9e7993f8e98
Nikolina <71445284+nikolina2k@users.noreply.github.com>
2023-04-27 22:48:33 +0300

    fill the about page with info about the project

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,19 +1,53 @@
-import React from 'react'
-import { Typography } from 'antd'
+import React from "react";
+import { Typography } from "antd";
 
 const { Title, Paragraph, Text, Link } = Typography;
 
+export default function About() {
+  return (
+    <>
+      <Typography>
+        <Title>The analytics dashboard project</Title>
 
-export default function About () {
-    return (
-        <>
-            <Typography>
-               <Title>About</Title>
-
-               <Paragraph>
-                    TODO: write something about the protocol we're tracking and about the data and how we get it (if we had enough time)
-               </Paragraph>
-            </Typography>
-        </>
-    );
+        <Paragraph>
+          The world of decentralized finance has witnessed explosive growth in
+          recent times, and with this growth, the need for advanced analytics
+          tools to monitor the performance of investments has become
+          increasingly crucial. The analytics dashboard project aims to fulfill
+          this need and provide users with a comprehensive overview of their
+          investments on a decentralized exchange.
+        </Paragraph>
+        <Paragraph>
+          The dashboard is designed to be user-friendly and visually appealing,
+          making it easy for both novice and experienced users to navigate and
+          analyze their investment data. The platform displays key metrics such
+          as trading volume, liquidity, and price movements, which are crucial
+          for decision-making when managing a diversified portfolio.
+        </Paragraph>
+        <Paragraph>
+          One of the significant benefits of using this analytics dashboard is
+          that it enables users to track the performance of multiple assets and
+          pairs at the same time. This feature is particularly useful for
+          traders who have diverse portfolios and need to monitor various assets
+          simultaneously. Additionally, the platform provides users with
+          detailed historical data, enabling them to conduct in-depth analyses
+          and make informed decisions.
+        </Paragraph>
+        <Paragraph>
+          The analytics dashboard project is continuously evolving
+          and incorporating new features based on user feedback and market
+          trends. This approach ensures that the platform remains up-to-date and
+          relevant, offering users the best tools to manage their investments on
+          a decentralized exchange.
+        </Paragraph>
+        <Paragraph>
+          This project is a game-changer for
+          investors who are keen on tracking their investments in decentralized
+          finance. It provides users with real-time insights and data
+          visualization, which are essential for making informed investment
+          decisions in a highly dynamic market.
+        </Paragraph>
+      </Typography>
+    </>
+  );
 }
