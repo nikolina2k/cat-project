d427645f4146c02c045d7373c82730c67d3f2cba
Nikolina <71445284+nikolina2k@users.noreply.github.com>
2023-04-29 00:47:12 +0300

    fix styling of about page, tokens and pairs tables

--- src/pages/Pairs/index.tsx
+++ src/pages/Pairs/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import PairsTable, { Pair } from '../../components/PairsTabe';
-import './Pairs.css';
 import { useCategorizedData } from '../../contexts/GlobalData';
 
 export default function Pairs () {
     const { pairs } = useCategorizedData()
     return (
-        <div className="pairs-container">
+        <div>
             <PairsTable pairs={pairs} />
         </div>
     );
