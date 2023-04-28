f72942fd8f44b769621b022069104109d848cbd5
Sambura <superkirs2@gmail.com>
2023-04-29 00:11:58 +0300

    Remove icons from TokenTable

--- src/components/TokenTable/index.tsx
+++ src/components/TokenTable/index.tsx
@@ -1,17 +1,11 @@
 import React from "react";
 import { Table } from "antd";
-import { Image } from "antd";
 
 const { Column } = Table;
 
-interface TokenInfo {
-  name: string;
-  icon_url: string;
-}
-
 interface TokenTableEntry {
   key: React.Key;
-  tokenInfo: TokenInfo;
+  tokenName: string;
   volume24: number;
   symbol: string;
   liquidity: number;
@@ -21,16 +15,7 @@ interface TokenTableEntry {
 function TokenTable({ data }) {
   return (
     <Table dataSource={data} data-testid="token-table">
-      <Column
-        title="Name"
-        dataIndex="tokenInfo"
-        key="tokenInfo"
-        render={(tokenInfo: TokenInfo) => (
-          <>
-            <Image height={32} src={tokenInfo.icon_url} /> {tokenInfo.name}
-          </>
-        )}
-      />
+      <Column title="Name" dataIndex="tokenName" key="tokenName"/>
       <Column title="Volume (24 h)" dataIndex="volume24" key="volume24" />
       <Column title="Symbol" dataIndex="symbol" key="symbol" />
       <Column title="Liquidity" dataIndex="liquidity" key="liquidity" />
